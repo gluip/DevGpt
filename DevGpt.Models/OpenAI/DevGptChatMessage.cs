062bddc44bd9f12642e29068369d244df42f2cc5
LAPTOP-D7CU25FL\marti <martijn_muurman@hotmail.com>
2023-12-09 15:03:09 +0100

    Cleaned up

--- DevGpt.Models/OpenAI/DevGptChatMessage.cs
+++ DevGpt.Models/OpenAI/DevGptChatMessage.cs
@@ -4,7 +4,7 @@ using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using DevGpt.Models.Commands;
 using DevGpt.Models.OpenAI;
-using OpenAI.Chat;
+
 
 namespace DevGpt.Models.OpenAI;
 

043006c75c3ee7c5cc08553ce73cc5b361012af5
LAPTOP-D7CU25FL\marti <martijn_muurman@hotmail.com>
2023-12-12 22:46:07 +0100

    Added selenium

--- DevGpt.Commands.Web.Selenium/SeleniumBrowser.cs
+++ DevGpt.Commands.Web.Selenium/SeleniumBrowser.cs
@@ -3,6 +3,9 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Threading.Tasks;
+using DevGpt.Commands.Web.Services;
+using OpenQA.Selenium.DevTools;
+using OpenQA.Selenium.DevTools.V120.Network;
 using WebDriverManager.DriverConfigs.Impl;
 using WebDriverManager;
 
@@ -10,33 +13,50 @@ namespace DevGpt.Commands.Web.Selenium
 {
     public class SeleniumBrowser : IBrowser
     {
-        private IWebDriver driver;
+        private ChromeDriver _driver;
 
-        public SeleniumBrowser()
-        {
-            new DriverManager().SetUpDriver(new ChromeConfig());
-            driver = new ChromeDriver();
-        }
+           
 
         public async Task<string> OpenPage(string url)
         {
-            driver.Navigate().GoToUrl(url);
-            return await Task.FromResult(driver.PageSource);
+            new DriverManager().SetUpDriver(new ChromeConfig()); 
+            _driver = new ChromeDriver();
+           await ConfigureBlockedUrls();
+
+           _driver.Navigate().GoToUrl(url);
+
+
+
+
+            return await Task.FromResult(_driver.PageSource);
+        }
+
+        private async Task ConfigureBlockedUrls()
+        {
+            var devTools = _driver.GetDevToolsSession();
+            var domains = devTools.GetVersionSpecificDomains<OpenQA.Selenium.DevTools.V120.DevToolsSessionDomains>();
+            await domains.Network.Enable(new EnableCommandSettings());
+            
+            var blockedURLs = BrowserHelper.GetBlockedUrls().Select(url => $"{url}*").ToArray();
+            await domains.Network.SetBlockedURLs(new SetBlockedURLsCommandSettings
+            {
+                Urls = blockedURLs
+            });
         }
 
         public async Task<string> GetPageHtml()
         {
-            return await Task.FromResult(driver.PageSource);
+            return await Task.FromResult(BrowserHelper.CleanHtml(_driver.PageSource));
         }
 
         public async Task<string> GetPageText()
         {
-            return await Task.FromResult(driver.FindElement(By.TagName("body")).Text);
+            return await Task.FromResult(_driver.FindElement(By.TagName("body")).Text);
         }
 
         public async Task FillAsync(string selector, string value)
         {
-            var element = driver.FindElement(By.CssSelector(selector));
+            var element = _driver.FindElement(By.CssSelector(selector));
             element.Clear();
             element.SendKeys(value);
             await Task.CompletedTask;
@@ -44,14 +64,14 @@ namespace DevGpt.Commands.Web.Selenium
 
         public async Task ClickAsync(string locator)
         {
-            var element = driver.FindElement(By.CssSelector(locator));
+            var element = _driver.FindElement(By.CssSelector(locator));
             element.Click();
             await Task.CompletedTask;
         }
 
         public async Task<string> TakeScreenshot()
         {
-            var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+            var screenshot = ((ITakesScreenshot)_driver).GetScreenshot();
             var screenshotName = $"screenshot-{DateTime.Now:yyyyMMddHHmmss}.png";
 
             screenshot.SaveAsFile(screenshotName);
@@ -60,7 +80,7 @@ namespace DevGpt.Commands.Web.Selenium
 
         public async Task<string> TakeBase64Screenshot()
         {
-            var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+            var screenshot = ((ITakesScreenshot)_driver).GetScreenshot();
             return await Task.FromResult(screenshot.AsBase64EncodedString);
         }
     }
---
+++ DevGpt.Commands/Commands/GetCurrentDateCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevGpt.Models.Commands;
+
+namespace DevGpt.Commands.Commands
+{
+    public class GetCurrentDateCommand:ICommand
+    {
+        public string Name => "get_current_date";
+        public string Execute(string[] args)
+        {
+            return DateTime.Today.ToString("D");
+
+        }
+
+        public string[] Arguments => Array.Empty<string>();
+        public string Description => "Gets the current date";
+
+      
+    }
+}
